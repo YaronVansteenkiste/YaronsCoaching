b98ef8aa443804bc5a5737895e53b6034e4aa0bb
yaronvsk <yaronvsk@googlemail.com>
2023-11-05 18:33:39 +0100

    05/11: performance

--- YaronsCoaching/src/pages/home.js
+++ YaronsCoaching/src/pages/home.js
@@ -6,7 +6,6 @@ import photo5 from "../images/photo5.png";
 import { FaInstagram, FaLinkedin, FaDiscord } from 'react-icons/fa';
 import gsap from 'gsap';
 
-
 function Home() {
     const images = useMemo(() => [photo1, photo4, photo5], []);
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -15,19 +14,22 @@ function Home() {
         const animateText = () => {
             const textElements = document.querySelectorAll('.text-to-reveal');
             gsap.from(textElements, {
-              opacity: 0,
-              x: -20,
-              duration: 1,
-              stagger: 0.1,
-              ease: 'power1.inOut',
+                opacity: 0,
+                x: -20,
+                duration: 1,
+                stagger: 0.1,
+                ease: 'power1.inOut',
             });
-          };
+        };
 
         animateText();
+    }, []); // This useEffect will run only once when the component mounts
+
+    useEffect(() => {
         const interval = setInterval(() => {
             const nextIndex = (currentIndex + 1) % images.length;
             setCurrentIndex(nextIndex);
-        }, 15000);
+        }, 6000);
 
         return () => {
             clearInterval(interval);
@@ -35,10 +37,9 @@ function Home() {
     }, [currentIndex, images]);
 
     const customTransition = {
-        type: "tween", 
-        duration: 1, 
+        type: "tween",
+        duration: 1,
     };
-
 
     return (
         <div className="profile-section" id='home'>
@@ -68,13 +69,13 @@ function Home() {
                     </p>
                 </div>
                 <div className="socials-horizontal">
-                <a href="https://www.instagram.com/yaronvsk/"
-                    target="_blank" rel="noreferrer"><FaInstagram /></a>
-                <a href="https://linkedin.com/in/yaron-vansteenkiste/"
-                    target="_blank" rel="noreferrer"><FaDiscord /></a>
-                <a href="https://github.com/YaronVansteenkiste"
-                    target="_blank" rel="noreferrer"><FaLinkedin /></a>
-            </div>
+                    <a href="https://www.instagram.com/yaronvsk/"
+                        target="_blank" rel="noreferrer"><FaInstagram /></a>
+                    <a href="https://linkedin.com/in/yaron-vansteenkiste/"
+                        target="_blank" rel="noreferrer"><FaDiscord /></a>
+                    <a href="https://github.com/YaronVansteenkiste"
+                        target="_blank" rel="noreferrer"><FaLinkedin /></a>
+                </div>
                 <div className="controls text-to-reveal">
                     <a href='#contact'>CONTACTEER MIJ</a>
                     <a href='#contact' className='email'>Of stuur mij een mail.</a>
@@ -83,7 +84,7 @@ function Home() {
             <div className="profile-photo text-to-reveal">
                 <AnimatePresence>
                     <motion.img
-                        key={images[currentIndex]} 
+                        key={images[currentIndex]}
                         initial={{ opacity: 0, y: -20 }}
                         animate={{ opacity: 1, y: -20 }}
                         exit={{ opacity: 0, y: -20 }}
